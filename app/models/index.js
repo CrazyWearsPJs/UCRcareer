8f92674e98b896b65420f4b74715e80128266981
omoman <mannysanchez998@gmail.com>
2014-10-30 22:01:51 +0000

    Expose applicant and employer constructors

--- app/models/index.js
+++ app/models/index.js
@@ -7,16 +7,42 @@ var mongoose = require('mongoose');
 var applicantSchema = require('./schema.applicant')
   , employerSchema  = require('./schema.employer');
 
+var dbRef = undefined;
+
 /**
- * Register models
+ * Register models and sets local db reference
  * @param db {Connection} Mongoose connection
  * @return none
- * @api public
  */
 
 function register (db) {
-    db.model('Applicant', applicantSchema);
-    db.model('Employer' , employerSchema);
+    dbRef = db;
+	dbRef.model('Applicant', applicantSchema);
+    dbRef.model('Employer' , employerSchema);
+}
+
+/**
+ * creates an applicant model constructor
+ * @param none
+ * @return {Model}
+ */
+
+function applicant () {
+	if (dbref === undefined)
+		throw new error('Need to register models first!');
+	return dbref.model('Applicant');
+}
+
+/**
+ * creates an employer model constructor
+ * @param none
+ * @return {Model}
+ */
+
+function employer () {
+	if (dbref === undefined)
+		throw new error('Need to register models first!');
+	return dbref.model('Employer');
 }
 
 /**
@@ -24,5 +50,7 @@ function register (db) {
  */
 
 module.exports = {
-    register: register
+    register:  register
+  , applicant: applicant
+  , employer:  employer
 };
