8761ceee6a119fe111e13f65161a3939ecc19602
omoman <mannysanchez998@gmail.com>
2014-10-31 12:10:30 -0700

    Split creation operation out of index.js

--- app/accounts/index.js
+++ app/accounts/index.js
@@ -2,36 +2,11 @@
  * Module Dependencies
  */
  
-var models    = require('../models')
-  , Applicant = models.applicant()
-  , Employer  = models.employer();
+var Create = require('./create');
 
 /**
- * Create a new applicant in our db
- * @param applicantInfo {Object} Should match schema
- * @return none
- */
-
-function createApplicant (applicantInfo) {
-	var johnDoe = new Applicant(applicantInfo);
-	johnDoe.save();
-}
-
-/**
- * Create a new employer in our db
- * @param employerInfo {Object} Should match schema
- * @return none
- */
-
-function createEmployer (employerInfo) {
-	var janeDoe = new Employer(employerInfo);
-	janeDoe.save();
-}
-
-/**
- * Exports
+ * CRUD exports
  */
 module.exports = {
-	createApplicant : createApplicant
-  , createEmployer  : createEmployer
+    Create : Create
 }
