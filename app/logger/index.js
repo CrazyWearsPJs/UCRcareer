eb276c153384bd766767410226d106e576efbdf7
omoman <mannysanchez998@gmail.com>
2014-11-01 12:16:02 -0700

    Logger doesn't exit on error now and enabled pretty print

--- app/logger/index.js
+++ app/logger/index.js
@@ -17,37 +17,45 @@ var serverSettings = config.serverSettings
   , appWarnLogPath = path.join(__dirname, serverSettings.appWarnLogFile);
 
 var logger = new winston.Logger({
-	transports: [
+    transports: [
         // By specifying info as the level, warn and
-		// error will also be logged to the console
-		new winston.transports.Console({ 
-			level:     'info'
-		  , colorize:  true
-		})
-		// Info file logging
-	  , new winston.transports.File({
-	  		level:     'info'
-		  , filename:  appInfoLogPath
-		  , timestamp: true
-		  , json:      false
-		  , name:      'log.info'
-		})
-		// Warn file logging 
-	  , new winston.transports.File({
-	  		level:     'warn'
-		  , filename:  appWarnLogPath
-		  , timestamp: true
-		  , json:      false
-		  , name:      'log.warn'
-		})
-		// Error file logging
-	  , new winston.transports.File({
-	  		level:     'error'
-		  , filename:  appErrLogPath
-		  , timestamp: true
-		  , json:      false
-		  , name:      'log.err'
-		})
+        // error will also be logged to the console
+        new winston.transports.Console({ 
+            level:       'info'
+          , colorize:    true
+          , exitOnError: false
+          , prettyPrint: true
+        })
+        // Info file logging
+      , new winston.transports.File({
+            level:       'info'
+          , filename:    appInfoLogPath
+          , timestamp:   true
+          , json:        false
+          , exitOnError: false
+          , prettyPrint: true
+          , name:        'log.info'
+        })
+        // Warn file logging 
+      , new winston.transports.File({
+            level:       'warn'
+          , filename:    appWarnLogPath
+          , timestamp:   true
+          , json:        false
+          , exitOnError: false
+          , prettyPrint: true
+          , name:        'log.warn'
+        })
+        // Error file logging
+      , new winston.transports.File({
+            level:       'error'
+          , filename:    appErrLogPath
+          , timestamp:   true
+          , json:        false
+          , prettyPrint: true
+          , exitOnError: false
+          , name:        'log.err'
+        })
     ]
 });
 
