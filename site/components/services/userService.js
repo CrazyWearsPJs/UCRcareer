70ee6b455600161d155324b477037f4428526948
Alex Gonzales <agonz056@ucr.edu>
2014-11-03 14:03:20 -0500

    pulled out copyNonNull logic, applied to getProfileData and getAll

--- site/components/services/userService.js
+++ site/components/services/userService.js
@@ -2,10 +2,12 @@ angular.module('ucrCareerServices')
     .factory('User', [ function() {
         var forEach = angular.forEach,
             isFunction = angular.isFunction,
+            isObject = angular.isObject,
             copy = angular.copy;
         
         var PROFILE_DATA_FIELDS = ['personal', 'contact', 'spec',
             'location']; 
+
 
         var User = {
             'credentials': {
@@ -43,6 +45,23 @@ angular.module('ucrCareerServices')
             }
         };
 
+        var copyNonNull = function(src, dest) { 
+            if(dest) {
+                copy(src, dest);
+            } else {
+                dest = copy(src); 
+            }
+
+            if(isObject(dest)) {
+                forEach(dest, function(value, key) {
+                    if(!value) {
+                        delete dest[key];
+                    }
+                });
+            }
+            return dest;
+        };
+
         User.addCredentials = function(email, password) {
             this.credentials.email = email;
             this.credentials.password = password;
@@ -64,26 +83,19 @@ angular.module('ucrCareerServices')
             var info = {};
             forEach(User, function(value, key){
                 if(!isFunction(value)) {
-                    info[key] = value; 
+                    info[key] = copyNonNull(value); 
                 }
             });
             return info;
         };
 
         User.getProfileData = function() {
-            var info = {},
-                noNullValues = null;
+            var info = {};
             forEach(User, function(_, key) {
                 forEach(PROFILE_DATA_FIELDS, function(value,
                     profileDataField) {
                     if(key === profileDataField) {
-                        noNullValues = copy(value);
-                        forEach(nullValues, function(copyValue, copyKey) {
-                            if(!copyValue) {
-                                delete noNullValues[copyKey];
-                            }
-                        });
-                        info[profileDataField] = noNullValues;
+                        info[profileDataField] = copyNonNull(value);
                     }
                 });  
             });
