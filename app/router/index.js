1cf0c7900840f749e03df84fd9301aae83a83a66
Alex Gonzales <agonz056@ucr.edu>
2014-11-04 03:30:46 -0500

    added sessions and auth and error middleware

--- app/router/index.js
+++ app/router/index.js
@@ -1,3 +1,27 @@
+var checkAuthApplicant = function(req, res, next) {
+    if(req.session.applicantUserId) {
+        next();
+    } else {
+        var err = new Error("Forbidden");
+        err.name = "error";
+        err.status = 403;
+        next(err);
+    }
+};
+
+
+
 module.exports = function(app, db) {
     app.use('/api/v1/register', require('./routes/register')(db));
+    /*
+     * Error middleware
+     */
+    app.use(function(err, req, res, next) {
+        if(err.status >= 200 && err.status < 300) {
+            next();
+        }
+        var errorObj = {};
+        errorObj[err.name] = err.message;
+        res.status(err.status || 500).json(errorObj);
+    });
 };
--- app/router/routes/register.js
+++ app/router/routes/register.js
@@ -3,8 +3,28 @@ var express = require('express');
 module.exports = function(db) {
     var router = express.Router();
 
-    router.post('/applicant', function(req, res) {
-         
+    router.post('/applicant', function(req, res, next) {
+        var Applicant = db.model('Applicant'),
+            newApplicant = null,
+            applicantData = req.body;
+        
+        if(applicantData && newApplicant.credentials) {
+            newApplicant = new Applicant(req.body);
+            newApplicant.save(function(err, newApplicantUpdated) {
+                if(err) {
+                    err.status = 400;
+                    next(err);
+                } else {
+                    req.session.applicantUserId = newApplicantUpdated._Id;
+                    res.status(200).end();
+                }
+            });
+        } else {
+            var err = new Error('Missing applicant credentials');            
+            err.name = 'error';
+            err.status = 400;
+            next(err);
+        }
     });
 
     router.post('/employer', function(req, res) {
