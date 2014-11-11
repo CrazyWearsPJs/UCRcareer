4c05591080083814d62ea172b987a67a138cf7de
omoman <mannysanchez998@gmail.com>
2014-11-11 15:41:48 -0800

    Added build css to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,14 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var traceur = require('gulp-traceur');
 var karma = require('karma');
 var jshint = require('gulp-jshint');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
-/*
-var handle = function(stream) {
-    stream.on('error', function() {
-        gutil.log.apply(this, arguments);
-        stream.end();
-    });
-    
-};
-*/
+
 var sources = {
     js: ['site/app.js', 'site/components/**/*.js'],
     scss: ['site/styles/**/*.scss']
@@ -26,8 +19,18 @@ var sources = {
 var destinations = {
     js: 'site/build/js/',
     css: 'site/build/css/'
-
 };
+
+gulp.task('build:css', function(){
+    return gulp.src(sources.scss)
+        .pipe(sass())
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest(destinations.css));
+});
+
+gulp.task('watch:css', function() {
+    gulp.watch(sources.scss, ['build:css']);
+});
 
 gulp.task('build:js', function(){
     return gulp.src(sources.js)
@@ -40,19 +43,10 @@ gulp.task('build:js', function(){
 
 });
 
-/*
-gulp.task('test:js', function(done) {
-    return karma.start({
-        configFile: __dirname + 'karma.conf.js',
-        singleRun: true
-    }, done);
-}); */
-
-
 gulp.task('lint:js', function() {
     return gulp.src(sources.js)
-            .pipe(jshint('.jshintrc'))
-            .pipe(jshint.reporter('jshint-stylish'));
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('clean:js', function() {
@@ -66,5 +60,5 @@ gulp.task('watch:js',  function() {
 
 gulp.task('make:js', ['clean:js', 'lint:js', 'build:js']);
 
-gulp.task('default', ['make:js', 'watch:js']);
+gulp.task('default', ['make:js', 'build:css', 'watch:css', 'watch:js']);
 
