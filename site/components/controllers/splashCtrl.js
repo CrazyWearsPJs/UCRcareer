d26e9b72b5ea46b50fbbdf11c899e004a24a2b55
Noel <nllan001@ucr.edu>
2014-11-12 20:02:14 +0000

    added a parse to the search bar so that the search input is now put into an array

--- site/components/controllers/splashCtrl.js
+++ site/components/controllers/splashCtrl.js
@@ -1,2 +1,17 @@
 angular.module('ucrCareerControllers')
-    .controller('SplashCtrl', ['$scope', function($scope){}]);
+    .controller('SplashCtrl', ['$scope', '$location', function SplashCtrl($scope, $location){
+        $scope.splash = {
+            'search': []
+        };
+        $scope.ok = function() {
+            if($scope.splash.search[0]) {
+                var search = $scope.splash.search.toString();
+                $scope.splash.search = search.split(" ");
+console.log($scope.splash.search);
+                // TODO redirect to search page
+            }
+            else {
+                $location.path('/');
+            }
+        };
+}]);
