265c1ac4b56bfefab039edd75c83ae72ae0c0834
omoman <mannysanchez998@gmail.com>
2014-11-13 13:26:15 -0800

    Added error logging to register route and started spliting up views
    Used logger.error in register route. Also started to split up views
    into smaller chunks

--- app/router/routes/register.js
+++ app/router/routes/register.js
@@ -1,73 +1,87 @@
+/**
+ * Module Dependancies
+ */
+
 var express = require('express'),
     models = require('../../models');
 
-    var router = express.Router();
+var router = express.Router()
+  , logger = require('../../logger');
 
-    /*
-     * Route logic for registering an applicant
-     * @param req {Object} 
-     *  @field req.body, profile data for an applicant, 
-     *      should have fields that allowed by the schema in schema.applicant.js
-     *
-     * @param res {Object} the response object
-     * @param next {Function} callback, used to send errors to the Error Middleware
-     *
-     */
-    router.post('/applicant', function(req, res, next) {
-        var Applicant = models.applicant(),
-            newApplicant = null,
-            applicantData = req.body;
-        if(applicantData && applicantData.credentials) {
-            newApplicant = new Applicant(applicantData);
-            newApplicant.save(function(err, newApplicantUpdated) {
-                if(err) {
-                    err.status = 400;
-                    next(err);
-                } else {
-                    req.session.applicantUserId = newApplicantUpdated._id;
-                    res.status(200).end();
-                }
-            });
-        } else {
-            var err = new Error('Missing applicant credentials');            
-            err.name = 'error';
-            err.status = 400;
-            next(err);
-        }
-    });
+/*
+ * Route logic for registering an applicant
+ * @param req {Object} 
+ *  @field req.body, profile data for an applicant, 
+ *      should have fields that allowed by the schema in schema.applicant.js
+ *
+ * @param res {Object} the response object
+ * @param next {Function} callback, used to send errors to the Error Middleware
+ *
+ */
+router.post('/applicant', function(req, res, next) {
+    var Applicant = models.applicant(),
+        newApplicant = null,
+        applicantData = req.body;
+    if(applicantData && applicantData.credentials) {
+        newApplicant = new Applicant(applicantData);
+        newApplicant.save(function(err, newApplicantUpdated) {
+            if(err) {
+                err.status = 400;
+                logger.error(err);
+                next(err);
+            } else {
+                req.session.applicantUserId = newApplicantUpdated._id;
+                res.status(200).end();
+            }
+        });
+    } else {
+        var err = new Error('Missing applicant credentials');            
+        err.name = 'error';
+        err.status = 400;
+        logger.error(err);
+        next(err);
+    }
+});
+
+/*
+* Route logic for registering an applicant
+* @param req {Object} 
+*  @field req.body, profile data for an employer, 
+*      should have fields that allowed by the schema in schema.employer.js
+*
+* @param res {Object} the response object
+* @param next {Function} callback, used to send errors to the Error Middleware
+*
+*/
+router.post('/employer', function(req, res, next) {
+    var Employer = models.employer(),
+        newEmployer = null,
+        employerData = req.body;
    
-   /*
-    * Route logic for registering an applicant
-    * @param req {Object} 
-    *  @field req.body, profile data for an employer, 
-    *      should have fields that allowed by the schema in schema.employer.js
-    *
-    * @param res {Object} the response object
-    * @param next {Function} callback, used to send errors to the Error Middleware
-    *
-    */
-    router.post('/employer', function(req, res, next) {
-        var Employer = models.employer(),
-            newEmployer = null,
-            employerData = req.body;
-       
-        if(employerData && employerData.credentials) {
-            newEmployer = new Employer(employerData);
-            newEmployer.save(function(err, newEmployerUpdated) {
-                if(err) {
-                    err.status = 400;
-                    next(err);
-                } else {
-                    req.session.employerUserId = newEmployerUpdated._id;
-                    res.status(200).end();
-                }
-            });
-        } else {
-            var err = new Error('Missing employer credentials');            
-            err.name = 'error';
-            err.status = 400;
-            next(err);
-        }
-    });
+    if(employerData && employerData.credentials) {
+        newEmployer = new Employer(employerData);
+        newEmployer.save(function(err, newEmployerUpdated) {
+            if(err) {
+                err.status = 400;
+                logger.error(err);
+                next(err);
+            } else {
+                req.session.employerUserId = newEmployerUpdated._id;
+                res.status(200).end();
+            }
+        });
+    } else {
+        var err = new Error('Missing employer credentials');            
+        err.name = 'error';
+        err.status = 400;
+        logger.error(err);
+        next(err);
+    }
+});
 
-module.exports = router;
+
+/**
+ * Export router
+ */
+
+exports = module.exports = router;
