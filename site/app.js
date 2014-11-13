50f4a4f865300752df8816c6f42426ab48d5b443
David <dcastil1983@gmail.com>
2014-11-13 05:40:37 +0000

    Added html and controllers for updating profile page. Also added links on profile page to take you to update profile page

--- site/app.js
+++ site/app.js
@@ -10,11 +10,11 @@ angular.module('ucrCareer', ['ngRoute', 'ngMessages', 'ucrCareerControllers', 'u
                 templateUrl: 'templates/applicantRegister.html',
                 controller: 'ApplicantRegisterCtrl'
             }).when('/employerRegister', {
-   	            templateUrl: 'templates/employerRegister.html',
+   	        templateUrl: 'templates/employerRegister.html',
             	controller: 'EmployerRegisterCtrl'
             }).when('/jobPosting', {
-		        templateUrl: 'templates/jobPosting.html',
-        		controller: 'JobPostingCtrl'
+                templateUrl: 'templates/jobPosting.html',
+                controller: 'JobPostingCtrl'
             }).when('/employerProfile', {
                 templateUrl: 'templates/employerProfile.html',
                 controller: 'EmployerProfileCtrl'
@@ -24,6 +24,12 @@ angular.module('ucrCareer', ['ngRoute', 'ngMessages', 'ucrCareerControllers', 'u
             }).when('/thankyou', {
                 templateUrl: 'templates/thankyou.html',
                 controller: 'ThankyouCtrl'
+            }).when('/updateApplicantProfile', {
+                templateUrl: 'templates/updateApplicantProfile.html',
+                controller: 'UpdateApplicantProfileCtrl',
+            }).when('/updateEmployerProfile', {
+                templateUrl: 'templates/updateEmployerProfile.html',
+                controller: 'UpdateEmployerProfileCtrl',
             }).otherwise({
                     redirectTo: '/'
             });
--- site/components/controllers/employerProfileCtrl.js
+++ site/components/controllers/employerProfileCtrl.js
@@ -1,6 +1,14 @@
 angular.module('ucrCareerControllers')
     .controller('EmployerProfileCtrl', ['$scope', 'User', '$location',
     function EmployerProfileCtrl($scope, User, $location) {
+    $scope.showEmployer = function() {
+        return User.isEmployer();
+    };
+
+    $scope.updateEmployer = function() {
+        $location.path('/updateEmployerProfile');
+    };
+
     $scope.$on('$viewContentLoaded', function() {
         if(User.isEmployer()){
             var role = User.getUserRole();
