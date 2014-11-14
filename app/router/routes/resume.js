a804dad77564e99dc39c7b3098434fe0f0556b4d
omoman <mannysanchez998@gmail.com>
2014-11-14 02:27:17 -0800

    Can now view resume from applicant profile page

---
+++ app/router/routes/resume.js
@@ -0,0 +1,33 @@
+/**
+ * Module Dependancies
+ */
+
+var express = require('express')
+  , models  = require('../../models');
+
+var router    = new express.Router()
+  , Applicant = models.applicant(); 
+
+/**
+ * Returns users resume if they are authenticated
+ */
+
+router.get('/', function(req, res, next){
+    var applicantUserId = req.session.applicantUserId;
+    if (!applicantUserId)
+        return res.send(400, 'Forbidden');
+
+    // Find applicants resume
+    Applicant.findOne({_id : applicantUserId}, function(err, applicant){
+        if (!applicant)
+            return res.send(400, 'No Resume was uploaded');
+        res.sendFile(applicant.spec.resume);
+        console.log(applicant.spec.resume);
+    });
+});
+
+/**
+ * Export router
+ */
+
+exports = module.exports = router;
