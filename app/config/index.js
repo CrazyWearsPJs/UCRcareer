ca469ef31cff4e0c9b3cbbd062280b156c025020
omoman <mannysanchez998@gmail.com>
2014-11-30 18:52:39 -0800

    Using env variable for config

--- app/config/index.js
+++ app/config/index.js
@@ -4,27 +4,22 @@
 
 module.exports = {
     dbSettings: {
-	  host:     "10.0.2.15"
-  	, port:     "8081"
-  	, database: "UCRcareers"
-    }
-  , dbTestSettings: {
-	  host:     "10.0.2.15"
-	, port:     "8081"
-	, database: "TestUCRcareers"
+      host:     process.env.DB_HOST || "10.0.2.15"
+  	, port:     process.env.DB_PORT || "8081"
+  	, database: process.env.DB_NAME || "TestUCRcareers"
     }
   , serverSettings: {
-	  port:           "8080"
-	, httpErrLogFile: "HTTP.ERROR.log"
-	, httpLogFile:    "HTTP.log"
-	, appErrLogFile:  "APP.ERROR.log"
-	, appInfoLogFile: "APP.INFO.log"
-	, appWarnLogFile: "APP.WARN.log"
-	, staticPath:     "site"
+      port:           process.env.PORT || "8080"
+  	, httpErrLogFile: "HTTP.ERROR.log"
+  	, httpLogFile:    "HTTP.log"
+  	, appErrLogFile:  "APP.ERROR.log"
+  	, appInfoLogFile: "APP.INFO.log"
+  	, appWarnLogFile: "APP.WARN.log"
+  	, staticPath:     "site"
     , resumePath:     "/opt/resumes"
-    }
+  }
   , bcryptSettings: {
-	  hashRounds: 10
+	    hashRounds: 10
     }
 };
 
