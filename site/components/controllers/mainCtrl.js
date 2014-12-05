2b2f8646ef692114ed5dcad94e5f10dedb76d121
Alex Gonzales <agonz056@ucr.edu>
2014-12-05 14:09:09 -0500

    added toastr, need bower install

--- site/components/controllers/mainCtrl.js
+++ site/components/controllers/mainCtrl.js
@@ -1,12 +1,18 @@
 angular.module('huntEdu.controllers')
-    .controller('MainCtrl', ['$rootScope', 'socket', function($rootScope, socket){
+    .controller('MainCtrl', ['$rootScope', 'socket', '_', function($rootScope, socket, _){
         $rootScope.notifications = [];
        
+        var forEach = _.forEach;
         socket.on('multipleNotifications', function(data){
             $rootScope.notifications = data.notifications;
+            
+            forEach(data.notifications, function(notification) {
+                toastr.success(notification);
+            });
         });
 
         socket.on('singleNotification', function(data){
             $rootScope.notifications.push(data.notification);
+            toastr.success("data.notification");
         });    
     }]);
